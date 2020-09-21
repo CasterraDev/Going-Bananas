7e750fbc3ed24c46ed96961cb4c583677085865a
CasterraDev <casterradev@gmail.com>
2020-09-21 11:18:27 -0400

    basic platformer stuff

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public bool followPlayer;
+
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followPlayer)
+        {
+            transform.position = Vector2.Lerp(transform.position,new Vector2(player.transform.position.x,transform.position.y),1f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float spd = 10f;
+    public float jumpForce = 5f;
+    public LayerMask whatIsGround;
+
+    private BoxCollider2D boxCol;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCol = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector2(h * spd * Time.deltaTime,0));
+
+        if (IsGrounded() && Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * jumpForce;
+        }
+
+        if (h > 0 && !facingRight) Flip();
+        if (h < 0 && facingRight) Flip();
+    }
+
+    bool IsGrounded()
+    {
+        float extraHeight = .1f;
+        RaycastHit2D raycast = Physics2D.BoxCast(boxCol.bounds.center, boxCol.bounds.size, 0, Vector2.down, boxCol.bounds.extents.y + extraHeight, whatIsGround);
+        return raycast.collider != null;
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+}
