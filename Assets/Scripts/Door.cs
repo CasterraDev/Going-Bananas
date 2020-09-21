f1c263b8f829a964f017fb0c26344a0390d6664a
CasterraDev <mdgreen155@yahoo.com>
2020-09-21 11:18:27 -0400

    basic platformer stuff

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public int bananasToUnlock = 3;
+
+    private GameMaster gm;
+    private bool hasEnough;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasEnough = false;
+        gm = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+        Text text = GameObject.Find("UnlockText").GetComponent<Text>();
+        text.text = bananasToUnlock.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (gm != null) gm = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+        if (Input.GetKeyDown(KeyCode.E) && gm.bananas >= bananasToUnlock && collision.CompareTag("Player"))
+        {
+            Debug.Log("Next Scene");
+            gm.bananas -= bananasToUnlock;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    public int bananas = 0;
+    public GameObject bananaTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bananaTxt = GameObject.Find("BananaText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bananas < 0) bananas = 0;
+        bananaTxt.GetComponent<Text>().text = bananas.ToString();
+    }
+}
